f5a642515fbe8814c50969d0ccd8803bd1a1a710
mental_disaster <ghgh9244@gmail.com>
2023-05-19 20:32:24 +0900

    inital commit

---
+++ android/app/src/main/java/com/calendar/my_calendar/MainActivity.java
@@ -0,0 +1,6 @@
+package com.calendar.my_calendar;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:my_calendar/view/SampleView.dart';
+
+void main() {
+  runApp(const SampleView());
+}
