3800df77254934e12fe4597b8dfd3a5667f0929f
GH <gh.lim@promptech.co.kr>
2023-06-01 14:26:55 +0900

    add table_calendar lib and sample table

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:my_calendar/view/sample_view.dart';
+import 'package:my_calendar/view/table_sample.dart';
 
 void main() {
-  runApp(const SampleView());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TableBasicsExample(),
+    );
+  }
 }
